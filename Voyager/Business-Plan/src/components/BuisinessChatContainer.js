5cbe06448855fe57a8bb1fc7beadbf22433f6486
aminekefi <aminekefi045@gmail.com>
2024-02-13 13:30:38 +0100

    refactor to chakra components

--- Voyager/Business-Plan/src/components/BuisinessChatContainer.js
+++ Voyager/Business-Plan/src/components/BuisinessChatContainer.js
@@ -18,7 +18,15 @@ function BuisinessChatContainer() {
 
   return (
     <Flex direction="column" align="center" justify="center" className="App">
-      <Flex direction="column" align="center" justify="center" className="chat">
+      <Flex
+        direction="column"
+        align="flex-start"
+        justify="flex-start"
+        className="chat"
+        height="80%"
+        overflowY="scroll"
+        padding="10px"
+      >
         {messages.map((message, index) => (
           <div
             key={index}
@@ -28,13 +36,31 @@ function BuisinessChatContainer() {
           </div>
         ))}
       </Flex>
-      <Flex direction="row" align="center" justify="center" className="input-container">
+      <Flex direction="row" align="center" justify="center" className="input-container" height="20%" padding="10px">
         <Input
           type="text"
           value={input}
           onChange={(e) => setInput(e.target.value)}
+          width="70%"
+          padding="10px"
+          borderRadius="5px"
+          border="1px solid green"
+          outline="none"
         />
-        <Button onClick={handleMessageSubmit}>Send</Button>
+        <Button
+          onClick={handleMessageSubmit}
+          width="20%"
+          padding="10px"
+          borderRadius="5px"
+          border="none"
+          outline="none"
+          backgroundColor="#4caf50"
+          color="white"
+          cursor="pointer"
+          _hover={{ opacity: 0.8 }}
+        >
+          Send
+        </Button>
       </Flex>
     </Flex>
   );
