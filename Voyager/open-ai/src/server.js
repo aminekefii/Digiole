f910113c6814b9fb231e5950f94de7d88fcee83c
aminekefi <aminekefi045@gmail.com>
2024-02-15 17:39:35 +0100

    update openai configuration and run node server

--- Voyager/open-ai/src/server.js
+++ Voyager/open-ai/src/server.js
@@ -1,28 +1,22 @@
 const express = require("express");
-
 const cors = require("cors");
-
 const bodyParser = require("body-parser");
+const OpenAI = require("openai");
 
-const { Configuration, OpenAIApi } = require("openai");
+/*const config = new Configuration({
 
-const config = new Configuration({
-  apiKey: "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",
+});*/
+
+const openai = new OpenAI({
+    apiKey: "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",
 });
-
-const openai = new OpenAIApi(config);
-
-// Setup server
 
 const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-// endpoint for ChatGPT
-
 app.post("/chat", async (req, res) => {
   const { prompt } = req.body;
-
   const completion = await openai.createCompletion({
     model: "text-davinci-003",
     max_tokens: 512,
@@ -32,10 +26,8 @@ app.post("/chat", async (req, res) => {
   res.send(completion.data.choices[0].text);
 });
 
-const PORT = 8080;
+const PORT = 8020;
 
 app.listen(PORT, () => {
   console.log(`Server is running on port: ${PORT}`);
 });
-
-//run node server.js
