1f96ce80959a17ca4d64bdadc014171be8ca775f
aminekefi <aminekefi416@gmail.com>
2024-03-17 03:39:41 +0100

    change home page

--- Voyager-test/front-end/kefi_s_application1-v1.0.1/src/Routes.jsx
+++ Voyager-test/front-end/kefi_s_application1-v1.0.1/src/Routes.jsx
@@ -1,12 +1,11 @@
 import React from "react";
 import { useRoutes } from "react-router-dom";
-import Home from "pages/Home";
 import NotFound from "pages/NotFound";
 import VoyagervOne from "pages/VoyagervOne";
 
 const ProjectRoutes = () => {
   let element = useRoutes([
-    { path: "/", element: <Home /> },
+    { path: "/", element: <VoyagervOne /> },
     { path: "*", element: <NotFound /> },
     {
       path: "voyagervone",
