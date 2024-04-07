c155b8866c033d7abbaf8c514ddbec51ef928e29
aminekefi <aminekefi416@gmail.com>
2024-04-08 00:20:12 +0100

    add model + change upload icon fonc

--- Voyager-dev/Voyager-v1/FrontEnd/src/services/api/FilesUpload.js
+++ Voyager-dev/Voyager-v1/FrontEnd/src/services/api/FilesUpload.js
@@ -1,6 +1,9 @@
 import React, { useState } from "react";
 import axios from "axios";
 import { FiUpload } from 'react-icons/fi';
+import "../../styles/Modal.css";
+
+
 
 function FileUpload() {
     const [file, setFile] = useState(null);
@@ -21,18 +24,54 @@ function FileUpload() {
             });
     };
 
+
+
+const [modal, setModal] = useState(false);
+
+  const toggleModal = () => {
+    setModal(!modal);
+  };
+
+  if(modal) {
+    document.body.classList.add('active-modal')
+  } else {
+    document.body.classList.remove('active-modal')
+  }
+
+
+
     return (
         <>
-            <input
-                id="file-upload"
-                type="file"
-                style={{ display: 'none' }}
-                onChange={handleFileUpload}
-            />
-            <label htmlFor="file-upload"   >
+            
+            
+            <button type="button"   onClick={toggleModal}><label htmlFor="file-upload"   >
                 <FiUpload style={{ cursor: 'pointer', marginLeft: '10px', height:'50px'}} />
-            </label>
-            <button type="button" onClick={upload}>Upload</button>
+            </label></button>
+
+            {modal && (
+        <div className="modal">
+          <div onClick={toggleModal} className="overlay"></div>
+          <div className="modal-content">
+            <h2>Hello Modal</h2>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident
+              perferendis suscipit officia recusandae, eveniet quaerat assumenda
+              id fugit, dignissimos maxime non natus placeat illo iusto!
+              Sapiente dolorum id maiores dolores? Illum pariatur possimus
+              quaerat ipsum quos molestiae rem aspernatur dicta tenetur. Sunt
+              placeat tempora vitae enim incidunt porro fuga ea.
+            </p>
+            <button className="close-modal" onClick={toggleModal}>
+              CLOSE
+            </button>
+          </div>
+        </div>
+      )}
+
+
+
+
+
         </>
     );
 }
