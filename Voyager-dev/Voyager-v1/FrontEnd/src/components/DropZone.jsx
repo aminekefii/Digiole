38e5fe06b7dd399e2a2e94faab8ad1cd0ed9b661
aminekefi <aminekefi416@gmail.com>
2024-04-08 21:37:44 +0100

    add styles to dropzone

--- Voyager-dev/Voyager-v1/FrontEnd/src/components/DropZone.jsx
+++ Voyager-dev/Voyager-v1/FrontEnd/src/components/DropZone.jsx
@@ -22,9 +22,9 @@ const Dropzone = ({ className }) => {
   }, [])
 
   const { getRootProps, getInputProps, isDragActive } = useDropzone({
-    /*accept: {
+    accept: {
       'image/*': []
-    },*/
+    },
     maxSize: 1024 * 1000,
     onDrop
   })
@@ -66,100 +66,98 @@ const Dropzone = ({ className }) => {
   }
 
   return (
-    <form onSubmit={handleSubmit}>
-      <div
-        {...getRootProps({
-          className: className
-        })}
-      >
-        <input {...getInputProps()} />
-        <div className='flex flex-col items-center justify-center gap-4'>
-          <ArrowUpTrayIcon className='w-5 h-5 fill-current' />
-          {isDragActive ? (
-            <p>Drop the files here ...</p>
-          ) : (
-            <p>Drag & drop files here, or click to select files</p>
-          )}
-        </div>
-      </div>
-
-      {/* Preview */}
-      <section className='mt-10'>
-        <div className='flex gap-4'>
-          <h2 className='title text-3xl font-semibold'>Preview</h2>
-          <button
-            type='button'
-            onClick={removeAll}
-            className='mt-1 text-[12px] uppercase tracking-wider font-bold text-neutral-500 border border-secondary-400 rounded-md px-3 hover:bg-secondary-400 hover:text-white transition-colors'
-          >
-            Remove all files
-          </button>
-          <button
-            type='submit'
-            className='ml-auto mt-1 text-[12px] uppercase tracking-wider font-bold text-neutral-500 border border-purple-400 rounded-md px-3 hover:bg-purple-400 hover:text-white transition-colors'
-          >
-            Upload to Cloudinary
-          </button>
+    <main style={{ width: '100%', minHeight: '100vh', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+      <form onSubmit={handleSubmit} style={{ width: '24rem', color: '#4b5563', marginTop: '1.25rem', padding: '1rem', backgroundColor: '#ffffff', textAlign: 'center', borderRadius: '0.75rem', boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1), 0 6px 20px rgba(0, 0, 0, 0.19)' }}>
+        <div {...getRootProps({ className: className })}>
+          <input {...getInputProps()} />
+          <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', gap: '0.5rem' }}>
+            <ArrowUpTrayIcon className='w-5 h-5 fill-current' />
+            {isDragActive ? (
+              <p>Drop the files here ...</p>
+            ) : (
+              <p>Drag & drop files here, or click to select files</p>
+            )}
+          </div>
         </div>
 
-        {/* Accepted files */}
-        <h3 className='title text-lg font-semibold text-neutral-600 mt-10 border-b pb-3'>
-          Accepted Files
-        </h3>
-        <ul className='mt-6 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6 gap-10'>
-          {files.map(file => (
-            <li key={file.name} className='relative h-32 rounded-md shadow-lg'>
-        <img
-  src={file.preview}
-  alt={file.name}
-  className='h-full w-full object-contain rounded-md'
-  onLoad={() => {
-    URL.revokeObjectURL(file.preview);
-  }}
-/>
-              <button
-                type='button'
-                className='w-7 h-7 border border-secondary-400 bg-secondary-400 rounded-full flex justify-center items-center absolute -top-3 -right-3 hover:bg-white transition-colors'
-                onClick={() => removeFile(file.name)}
-              >
-                <XMarkIcon className='w-5 h-5 fill-white hover:fill-secondary-400 transition-colors' />
-              </button>
-              <p className='mt-2 text-neutral-500 text-[12px] font-medium'>
-                {file.name}
-              </p>
-            </li>
-          ))}
-        </ul>
+        {/* Preview */}
+        <section className='mt-10'>
+          <div className='flex gap-4'>
+            <h2 className='title text-3xl font-semibold'>Preview</h2>
+            <button
+              type='button'
+              onClick={removeAll}
+              className='mt-1 text-[12px] uppercase tracking-wider font-bold text-neutral-500 border border-secondary-400 rounded-md px-3 hover:bg-secondary-400 hover:text-white transition-colors'
+            >
+              Remove all files
+            </button>
+            <button
+              type='submit'
+              className='ml-auto mt-1 text-[12px] uppercase tracking-wider font-bold text-neutral-500 border border-purple-400 rounded-md px-3 hover:bg-purple-400 hover:text-white transition-colors'
+            >
+              Upload to Cloudinary
+            </button>
+          </div>
 
-        {/* Rejected Files */}
-        <h3 className='title text-lg font-semibold text-neutral-600 mt-24 border-b pb-3'>
-          Rejected Files
-        </h3>
-        <ul className='mt-6 flex flex-col'>
-          {rejected.map(({ file, errors }) => (
-            <li key={file.name} className='flex items-start justify-between'>
-              <div>
-                <p className='mt-2 text-neutral-500 text-sm font-medium'>
+          {/* Accepted files */}
+          <h3 className='title text-lg font-semibold text-neutral-600 mt-10 border-b pb-3'>
+            Accepted Files
+          </h3>
+          <ul className='mt-6 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6 gap-10'>
+            {files.map(file => (
+              <li key={file.name} className='relative h-32 rounded-md shadow-lg'>
+                <img
+                  src={file.preview}
+                  alt={file.name}
+                  className='h-full w-full object-contain rounded-md'
+                  onLoad={() => {
+                    URL.revokeObjectURL(file.preview)
+                  }}
+                />
+                <button
+                  type='button'
+                  className='w-7 h-7 border border-secondary-400 bg-secondary-400 rounded-full flex justify-center items-center absolute -top-3 -right-3 hover:bg-white transition-colors'
+                  onClick={() => removeFile(file.name)}
+                >
+                  <XMarkIcon className='w-5 h-5 fill-white hover:fill-secondary-400 transition-colors' />
+                </button>
+                <p className='mt-2 text-neutral-500 text-[12px] font-medium'>
                   {file.name}
                 </p>
-                <ul className='text-[12px] text-red-400'>
-                  {errors.map(error => (
-                    <li key={error.code}>{error.message}</li>
-                  ))}
-                </ul>
-              </div>
-              <button
-                type='button'
-                className='mt-1 py-1 text-[12px] uppercase tracking-wider font-bold text-neutral-500 border border-secondary-400 rounded-md px-3 hover:bg-secondary-400 hover:text-white transition-colors'
-                onClick={() => removeRejected(file.name)}
-              >
-                remove
-              </button>
-            </li>
-          ))}
-        </ul>
-      </section>
-    </form>
+              </li>
+            ))}
+          </ul>
+
+          {/* Rejected Files */}
+          <h3 className='title text-lg font-semibold text-neutral-600 mt-24 border-b pb-3'>
+            Rejected Files
+          </h3>
+          <ul className='mt-6 flex flex-col'>
+            {rejected.map(({ file, errors }) => (
+              <li key={file.name} className='flex items-start justify-between'>
+                <div>
+                  <p className='mt-2 text-neutral-500 text-sm font-medium'>
+                    {file.name}
+                  </p>
+                  <ul className='text-[12px] text-red-400'>
+                    {errors.map(error => (
+                      <li key={error.code}>{error.message}</li>
+                    ))}
+                  </ul>
+                </div>
+                <button
+                  type='button'
+                  className='mt-1 py-1 text-[12px] uppercase tracking-wider font-bold text-neutral-500 border border-secondary-400 rounded-md px-3 hover:bg-secondary-400 hover:text-white transition-colors'
+                  onClick={() => removeRejected(file.name)}
+                >
+                  remove
+                </button>
+              </li>
+            ))}
+          </ul>
+        </section>
+      </form>
+    </main>
   )
 }
 
