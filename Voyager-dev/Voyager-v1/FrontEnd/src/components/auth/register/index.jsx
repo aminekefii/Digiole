24b4ad846e0952ea81d90e9f360d64ef0c8c7a44
aminekefi <aminekefi416@gmail.com>
2024-04-09 16:09:37 +0100

    pass do not not match error

--- Voyager-dev/Voyager-v1/FrontEnd/src/components/auth/register/index.jsx
+++ Voyager-dev/Voyager-v1/FrontEnd/src/components/auth/register/index.jsx
@@ -20,11 +20,36 @@ const Register = () => {
     const onSubmit = async (e) => {
         e.preventDefault();
         if (!isRegistering) {
+            // Check if password and confirmPassword match
+            if (password !== confirmPassword) {
+                setErrorMessage("Passwords do not match.");
+                toast.error("Passwords do not match.", {
+                    position: "top-right",
+                    autoClose: 5000,
+                    hideProgressBar: false,
+                    closeOnClick: true,
+                    pauseOnHover: true,
+                    draggable: true,
+                    progress: undefined,
+                    theme: "light"
+                });
+                return; // Exit function if passwords don't match
+            }
+    
             setIsRegistering(true);
             try {
                 await doCreateUserWithEmailAndPassword(email, password);
                 // Show toast notification on successful registration
-                toast.success("Account created successfully!");
+                toast.success('Account created successfully!', {
+                    position: "top-right",
+                    autoClose: 5000,
+                    hideProgressBar: false,
+                    closeOnClick: true,
+                    pauseOnHover: true,
+                    draggable: true,
+                    progress: undefined,
+                    theme: "light",
+                });
                 // Redirect to login page after successful registration
                 navigate('/login');
             } catch (error) {
